e11361cdcbab05daffc135b2ac21347325f405d3
abdelilah-abdel <abdelilah2019ait@gmail.com>
2022-01-03 00:47:54 +0100

    sec
    .

---
+++ cmake-build-debug/CMakeFiles/Hospital.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Hospital.dir/main.cpp.obj"
+  "Hospital.exe"
+  "Hospital.exe.manifest"
+  "Hospital.pdb"
+  "libHospital.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Hospital.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
